586de7475b01f0345bf904a604dd22d55e69a662
Tobi J <github@mt-jaeger.de>
2025-07-06 23:20:14 +0200

    Auto

--- programm.py
+++ programm.py
@@ -5,19 +5,27 @@ from threading import Lock
 app = Flask(__name__)
 
 current_target_year = None
+current_duration=20
 year_lock = Lock()
+
 
 @app.route('/start', methods=["POST"])
 def zeitmaschine():
     zeitreise()
     if request.method == 'POST':
         year = request.form['year']
+        duration = request.form['duration']
     if not year or not year.lstrip("-").isdigit():
         return "Ungültige Eingabe", 400
     with year_lock:
         app.logger.error("Debug" + year)
         global current_target_year 
+        global current_duration
         current_target_year = int(year)
+        try:
+            current_duration = int(duration)
+        except (TypeError, ValueError):
+            current_duration = 20  # fallback
     return "Zeitreise gestartet", 200
     #return render_template('zeitmaschine.html', target_year=int(year))
 
@@ -27,7 +35,7 @@ def status():
     print(current_target_year)
     with year_lock:
         if current_target_year is not None:
-            return jsonify({"start": True, "year": current_target_year})
+            return jsonify({"start": True, "year": current_target_year, "duration": current_duration})
         else:
             return jsonify({"start": False})
 

b18a1837b096193e83428c0f9c520ebefc2ac02e
Tobi J <github@mt-jaeger.de>
2025-07-06 22:38:02 +0200

    Auto

--- programm.py
+++ programm.py
@@ -1,25 +1,51 @@
 from zeitmaschine import drei,vier,strob,drehlicht,zeitreise,standardbeleuchtung
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
+from threading import Lock
 
 app = Flask(__name__)
 
+current_target_year = None
+year_lock = Lock()
+
 @app.route('/zeitmaschine', methods=["POST"])
 def zeitmaschine():
-    #year = request.form.get("year")
+    zeitreise()
     if request.method == 'POST':
         year = request.form['year']
     if not year or not year.isdigit():
         return "Ungültige Eingabe", 400
-    return render_template('zeitmaschine.html', target_year=int(year))
+    with year_lock:
+        current_target_year = int(year)
+    return "Zeitreise gestartet", 200
+    #return render_template('zeitmaschine.html', target_year=int(year))
+
+@app.route("/status")
+def status():
+    with year_lock:
+        if current_target_year is not None:
+            return jsonify({"start": True, "year": current_target_year})
+        else:
+            return jsonify({"start": False})
+
+@app.route("/anzeige")
+def zeitanzeige():
+    return render_template("zeitmaschine.html")      
+    
+@app.route("/reset", methods=["POST"])
+def reset():
+    global current_target_year
+    with year_lock:
+        current_target_year = None
+    return "Reset OK", 200
 
 @app.route('/')
 def index():
     return render_template('konsole.html')
 
 @app.route("/stop", methods=["POST"])
-def stop_route():
+def stop():
     standardbeleuchtung()  # <-- Diese Funktion wird am Ende aufgerufen
-    return "stopped"
+    return jsonify({"status": "ok"})
 
 
 

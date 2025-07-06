b84781563a34c1553394f4aa21d551ff38feaca0
Tobi J <github@mt-jaeger.de>
2025-07-06 22:02:01 +0200

    Auto

--- programm.py
+++ programm.py
@@ -1,3 +1,12 @@
 from zeitmaschine import drei,vier,strob,drehlicht
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('zeitmaschine.html')
+
+
 
 drei(1)

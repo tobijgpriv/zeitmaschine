991366a0d971e33d12f0d2c4bfbda2900cfcbc88
Tobi J <github@mt-jaeger.de>
2025-07-06 21:18:12 +0200

    Auto

--- zeitmaschine.py
+++ zeitmaschine.py
@@ -6,17 +6,18 @@ from rpi_rf import RFDevice
 logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                     format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s',)
 
-repeat=10
-pulselength=350
-gpio=17
 
 
-def strob(state,repeat,pulselength,length):
-    protocol=1
-    if state == True:
-        code = 4433
-    else:
-        code = 4436
+def turn_switch_wrapper(code,state,protocol):
+    repeat=10
+    pulselength=350
+    gpio=17
+    length=24
+    turn_switch(state,repeat,pulselength,length,protocol,gpio,code)
+
+
+def turn_switch(state,repeat,pulselength,length,protocol,gpio,code):
+    
     
     rfdevice = RFDevice(gpio)
     rfdevice.enable_tx()
@@ -30,4 +31,13 @@ def strob(state,repeat,pulselength,length):
     rfdevice.tx_code(code, protocol, pulselength, length)
     rfdevice.cleanup()
 
-strob(True,repeat,pulselength,24)
+
+def strob(state):
+    if state == 1:
+        code = 4433
+    else:
+        code = 4436
+
+    turn_switch(code,state,1)
+
+strob(0)
